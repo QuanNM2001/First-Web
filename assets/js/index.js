952b883cf3b049e25c8f9252c8cb5bc1b05c9728
QuanNM2001 <minhquanqn2701@gmail.com>
2022-12-07 18:50:45 +0700

    new commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,27 @@
 window.onload = function() {
 
-    const currentdate = new Date(); 
-    var datetime = currentdate.getDate() + "/"
-        + (currentdate.getMonth()+1)  + "/" 
-        + currentdate.getFullYear() + " @ "  
-        + currentdate.getHours() + ":"  
-        + currentdate.getMinutes() + ":" 
-        + currentdate.getSeconds();
-        document.getElementById("datetime").innerHTML = datetime;
+    const currentdate = new Date();
+    var datetime = currentdate.getDate() + "/" +
+        (currentdate.getMonth() + 1) + "/" +
+        currentdate.getFullYear() + " @ " +
+        currentdate.getHours() + ":" +
+        currentdate.getMinutes() + ":" +
+        currentdate.getSeconds();
+    document.getElementById("datetime").innerHTML = datetime;
 
 }
-function openDrawer(){
+
+noClose = false;
+
+function openDrawer() {
     document.getElementById("drawer").style.display = "block";
     document.getElementById("drawer-child").style.width = "300px";
-
 }
-function closeDrawer(){
-    document.getElementById("drawer").style.display = "none";
-    document.body.style.backgroundColor = "white";
-}
-function exitDrawer(){
 
+function closeDrawer(event) {
+    if (noClose === false) {
+        document.getElementById("drawer").style.display = "none";
+        document.body.style.backgroundColor = "white";
+    } else
+        noClose = false;
 }
