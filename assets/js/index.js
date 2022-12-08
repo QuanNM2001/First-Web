d12293261a5e8a2ff811b7b4ba9053efc309dac4
quanNM2001 <minhquanqn2701@gmail.com>
2022-12-08 17:12:23 +0700

    new commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 window.onload = function() {
 
     const currentdate = new Date();
-    var datetime = currentdate.getDate() + "/" +
-        (currentdate.getMonth() + 1) + "/" +
-        currentdate.getFullYear() + " @ " +
+    var datetime = currentdate.getDate() + "-" +
+        (currentdate.getMonth() + 1) + "-" +
+        currentdate.getFullYear() + "  " +
         currentdate.getHours() + ":" +
         currentdate.getMinutes() + ":" +
         currentdate.getSeconds();
